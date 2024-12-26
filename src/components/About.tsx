e7d0a9742dc492abd19a45a1a46b820a88951802
Ahmed Bin Khalid <160387799+Prg-Strawy@users.noreply.github.com>
2024-12-26 08:47:38 +0500

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,7 +5,7 @@ const About = () => {
     <div className='container pt-32' id='about' data-aos="fade-left">
         <h2 className='text-4xl md:text-5xl' >About Me</h2>
         <p className='text-gray-500 pt-4'>
-            I am a student at GIAIC and, as well as at NED University, pursuing course of Artificial Intelligence and Electronics Engr. respectively. I'm constantly learning new everyday to be the best version of my self.
+            I am a student at GIAIC and, as well as at NED University, pursuing course of Artificial Intelligence and Electronics Engr. respectively. I&apos;m constantly learning new everyday to be the best version of my self.
         </p>
       
     </div>
